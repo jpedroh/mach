7209c26a487eaa9afb1b442ae6d889ad610f75d7
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2017-06-03 21:38:39 -0300

    Removed tests

--- js/briefing.js
+++ js/briefing.js
@@ -7,47 +7,22 @@ $('#partida_tbl').DataTable({
         search: "_INPUT_",
         searchPlaceholder: "Filtrar resultados",
         "info": "Mostrando _TOTAL_ de _MAX_ resultados",
-        "infoEmpty": "Não foram encontrados resultados",
+        "infoEmpty": "NĂŁo foram encontrados resultados",
         "infoFiltered": "(filtro aplicado)"
     }
 });
 
 //Inicializa as cartas de chegada
-/*$('#chegada_tbl').DataTable({
+$('#chegada_tbl').DataTable({
     paging: false,
     language: {
         search: "_INPUT_",
         searchPlaceholder: "Filtrar resultados",
         "info": "Mostrando _TOTAL_ de _MAX_ resultados",
-        "infoEmpty": "Não foram encontrados resultados",
+        "infoEmpty": "NĂŁo foram encontrados resultados",
         "infoFiltered": "(filtro aplicado)"
     }
-});*/
-
-    $('#example').DataTable( {
-        initComplete: function () {
-            this.api().columns().every( function () {
-                var column = this;
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo( $(column.footer()).empty() )
-                    .on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
- 
-                        column
-                            .search( val ? '^'+val+'$' : '', true, false )
-                            .draw();
-                    } );
- 
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
-        }
-    } );
-
-$('select').material_select();
+});
 
 //Ajusta a largura
 $('.dataTables_filter input').width($('.tabs').width());
