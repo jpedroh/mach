e58a6fe3bc0b7654c379e0d8ef167e0299b22a82
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2020-06-13 22:57:55 -0300

    Added constructor to VO

--- packages/core/src/vo/aircraft.ts
+++ packages/core/src/vo/aircraft.ts
@@ -4,4 +4,10 @@ export default class Aircraft {
   public readonly icaoCode: string;
   public readonly equipment: string;
   public readonly wakeTurbulence: AircraftWakeTurbulence;
+
+  public constructor(icaoCode: string, equipment: string, wakeTurbulence: AircraftWakeTurbulence) {
+    this.icaoCode = icaoCode;
+    this.equipment = equipment;
+    this.wakeTurbulence = wakeTurbulence;
+  }
 }
