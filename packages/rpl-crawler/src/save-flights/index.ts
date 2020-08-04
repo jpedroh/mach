4576884125e2bd3c7a611ee1f5c78e742c57954b
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2020-08-03 21:25:11 -0300

    Change connection configuration

--- packages/rpl-crawler/src/save-flights/index.ts
+++ packages/rpl-crawler/src/save-flights/index.ts
@@ -2,5 +2,14 @@ import makeSaveFlights from "./save-flights";
 import { Sequelize } from "sequelize";
 
 export default makeSaveFlights({
-    connection: new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:secret@172.17.0.2:5432/postgres')
+    connection: new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:secret@172.17.0.2:5432/postgres', {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
 })
