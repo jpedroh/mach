43c558ddbba732ac5593726ca3e3e7cf59e257ac
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2020-09-28 22:20:51 -0300

    Fix code style

--- packages/front/layouts/base-layout.tsx
+++ packages/front/layouts/base-layout.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 const BaseLayout: React.FC = ({ children }) => {
-  return <>
+  return (
+    <>
       <header>
         <h1>Mach</h1>
       </header>
-      <main>
-        {children}
-      </main>
-      <footer>
-      </footer>
-  </>
+      <main>{children}</main>
+      <footer></footer>
+    </>
+  )
 }
 
 export default BaseLayout
--- packages/front/pages/search-results.tsx
+++ packages/front/pages/search-results.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import BaseLayout from '../layouts/base-layout'
 
 const SearchResults: React.FC = () => {
-  return <BaseLayout>
-     <p>Teste</p>
-  </BaseLayout>
+  return (
+    <BaseLayout>
+      <p>Teste</p>
+    </BaseLayout>
+  )
 }
 
 export default SearchResults
