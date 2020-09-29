655adf352aeb37cd82a2c14c66f3b3e5c4f863d1
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2020-09-29 16:39:17 -0300

    Bump package versions

--- packages/front/components/footer-nav.tsx
+++ packages/front/components/footer-nav.tsx
@@ -9,7 +9,7 @@ const FooterNav: React.FC = () => {
       </Nav.Item>
       <Nav.Item>
         <Nav.Link href="https://www.github.com/jpedroh/mach" target="_blank">
-          Mach v4.0.0
+          Mach v4.1.0
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
