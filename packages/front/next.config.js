b51a64ea992ea6b0ff2499dff41f63b337191ca0
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2020-09-29 15:59:00 -0300

    Fix error on local server

--- packages/front/next.config.js
+++ packages/front/next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    basePath: '/mach',
-    assetPrefix: '/mach/',
+  basePath: isProd ? '/mach' : '',
+  basePath: isProd ? '/mach/' : ''
 }
