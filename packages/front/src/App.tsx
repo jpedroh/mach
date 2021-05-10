fa5dbbbc97b0a16e69bda98d533d18810c134917
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-05-09 22:32:44 -0300

    initial tailwind and react router config

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react'
+import { BrowserRouter, Switch } from 'react-router-dom'
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch></Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
