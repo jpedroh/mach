b96e7fe30563d979a148056235012dffb8f91c93
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-05-13 23:12:20 -0300

    build: remove lerna (#55)

--- release.config.js
+++ release.config.js
@@ -1,19 +1,14 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    ['semantic-release-lerna', { generateNotes: true, publish: false }],
+    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        message: 'chore(release): ${nextRelease.version} [skip release]',
-        assets: [
-          'CHANGELOG.md',
-          'lerna.json',
-          'package.json',
-          'packages/*/package.json'
-        ]
+        message:
+          'chore(release): ${nextRelease.version} [skip release]\n\n${nextRelease.notes}'
       }
     ]
   ],
