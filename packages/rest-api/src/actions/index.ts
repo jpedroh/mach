3d128dc0a4177faff7745556500191deab0d02f8
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-11-10 21:18:43 -0300

    feat: initial creation of rust package

--- packages/rest-api/src/actions/index.ts
+++
@@ -1,8 +0,0 @@
-import makeFindById from './find-by-id'
-import FlightUseCase from '../use-case'
-import makeFindAll from './find-all'
-
-export default {
-  findById: makeFindById(FlightUseCase),
-  findAll: makeFindAll(FlightUseCase)
-}
