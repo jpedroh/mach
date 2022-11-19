88da937ea3dc20f6c1e58c54f9f66fd5961f719e
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-11-19 16:11:57 -0300

    style: code formatting

--- packages/rpl-crawler/src/lib.rs
+++ packages/rpl-crawler/src/lib.rs
@@ -1,7 +1,7 @@
 mod has_rpl_update;
 
-use neon::prelude::*;
 use chrono::NaiveDate;
+use neon::prelude::*;
 use once_cell::sync::OnceCell;
 use tokio::runtime::Runtime;
 
@@ -19,16 +19,14 @@ fn has_rpl_update_wrapper(mut cx: FunctionContext) -> JsResult<JsPromise> {
     let date_as_string = cx.argument::<JsString>(0)?.value(&mut cx);
 
     let (deferred, promise) = cx.promise();
-    
+
     rt.spawn(async move {
         let date = NaiveDate::parse_from_str(date_as_string.as_str(), "%Y-%m-%d").unwrap();
         let has_rpl_update = has_rpl_update::has_rpl_update(date).await;
 
-        deferred.settle_with(&channel, move |mut cx| {
-            match has_rpl_update {
-                Ok(response) => Ok(cx.boolean(response)),
-                Err(error) => cx.throw_error(error)
-            }
+        deferred.settle_with(&channel, move |mut cx| match has_rpl_update {
+            Ok(response) => Ok(cx.boolean(response)),
+            Err(error) => cx.throw_error(error),
         });
     });
 
