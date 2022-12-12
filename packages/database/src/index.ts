89fbcd9a036b15ac201bbacabfa0e70e7b12ceb1
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-12-12 18:29:09 -0300

    chore: add dialect module information

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,9 +1,11 @@
 import { Sequelize } from 'sequelize'
 import FlightModelInitializer from './database-mapping'
+const pg = require('pg');
 
 export const connection = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
