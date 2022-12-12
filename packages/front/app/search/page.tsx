f18983c0b7c000518a56dc4b86896975ba7e1d81
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-12-12 19:08:36 -0300

    refactor: fetch data from DB direct in server component

--- packages/front/app/search/page.tsx
+++ packages/front/app/search/page.tsx
@@ -5,19 +5,12 @@ import Lead from '../../src/components/Lead'
 import GeneralLayout from '../../src/layouts/GeneralLayout'
 import { fetchFlights } from '../../src/services/fetch-flights'
 
-const getLeadMessage = (count: number) => {
-    return count === 1
-        ? 'There is a single result for your search.'
-        : `There are ${count} results for your search.`
-}
-
 export default async function Search({
     searchParams,
 }: {
     searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-
-    const flights = (await fetchFlights(searchParams as any))
+    const flights = await fetchFlights(searchParams ?? {});
 
     if (flights.length === 0) {
         return <GeneralLayout>
@@ -26,6 +19,12 @@ export default async function Search({
                 <Link href="/">Click here</Link> to make a new search.
             </Lead>
         </GeneralLayout>
+    }
+
+    const getLeadMessage = (count: number) => {
+        return count === 1
+            ? 'There is a single result for your search.'
+            : `There are ${count} results for your search.`
     }
 
     return (
