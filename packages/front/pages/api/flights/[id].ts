ccea938a4eded54bf61100f9369a99730c9e161c
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-12-12 18:17:36 -0300

    feat: rewrite API routes using NextJS API routes

---
+++ packages/front/pages/api/flights/[id].ts
@@ -0,0 +1,26 @@
+import { FlightModel } from "@mach/database";
+import type { NextApiRequest, NextApiResponse } from "next";
+import z from "zod";
+
+const schema = z.object({
+  id: z.string().uuid(),
+});
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405);
+  }
+
+  const data = schema.safeParse(req.query);
+
+  if (!data.success) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  const flight = await FlightModel.findByPk(data.data.id);
+  if (flight === null) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  res.status(200).json(flight);
+}
