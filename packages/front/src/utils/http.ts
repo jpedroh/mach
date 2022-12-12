690f4330631f2559f7924aa39fe8e27c2f445ad3
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-12-12 17:19:56 -0300

    chore: add logging

--- packages/front/src/utils/http.ts
+++ packages/front/src/utils/http.ts
@@ -6,6 +6,7 @@ function http<R = any, T = any>({
   query: T
 }): Promise<R> {
   const fullUrl = `${url}?${new URLSearchParams(query as any).toString()}`
+  console.log(`Making HTTP request to ${fullUrl}`);
 
   return fetch(fullUrl, { cache: "no-store" }).then((response) =>
     response.json()
