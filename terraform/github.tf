d5c60c7ecc698a113e98b6ac02a053cab7ff2f80
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-12-26 19:09:06 -0300

    rename variables and add missing variable

--- terraform/github.tf
+++ terraform/github.tf
@@ -8,12 +8,19 @@ data "github_actions_public_key" "mach_repository" {
 
 resource "github_actions_secret" "aisweb_api_key" {
   repository      = "mach"
-  secret_name     = "aisweb_api_key"
+  secret_name     = "AISWEB_API_KEY"
   plaintext_value = var.aisweb_api_key
 }
 
 resource "github_actions_secret" "aisweb_api_password" {
   repository      = "mach"
-  secret_name     = "aisweb_api_password"
+  secret_name     = "AISWEB_API_PASSWORD"
   plaintext_value = var.aisweb_api_password
+}
+
+
+resource "github_actions_secret" "database_url" {
+  repository      = "mach"
+  secret_name     = "DATABASE_URL"
+  plaintext_value = var.database_url
 }
