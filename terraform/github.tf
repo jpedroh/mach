21230059758ce69da9d1bad8ea4d7b967bfc392b
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-12-26 19:14:11 -0300

    ci: add missing env vars for build

--- terraform/github.tf
+++ terraform/github.tf
@@ -8,19 +8,19 @@ data "github_actions_public_key" "mach_repository" {
 
 resource "github_actions_secret" "aisweb_api_key" {
   repository      = "mach"
-  secret_name     = "AISWEB_API_KEY"
+  secret_name     = "aisweb_api_key"
   plaintext_value = var.aisweb_api_key
 }
 
 resource "github_actions_secret" "aisweb_api_password" {
   repository      = "mach"
-  secret_name     = "AISWEB_API_PASSWORD"
+  secret_name     = "aisweb_api_password"
   plaintext_value = var.aisweb_api_password
 }
 
 
 resource "github_actions_secret" "database_url" {
   repository      = "mach"
-  secret_name     = "DATABASE_URL"
+  secret_name     = "database_url"
   plaintext_value = var.database_url
 }
