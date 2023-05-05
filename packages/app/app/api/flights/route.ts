bac128778e3e42865ade22db515c1ce5dcaccc82
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-05-05 09:51:38 -0300

    refactor: migrate paginate flights route to app directory

---
+++ packages/app/app/api/flights/route.ts
@@ -0,0 +1,98 @@
+import { FlightModel } from "@mach/database";
+import { NextResponse } from "next/server";
+import { Op } from "sequelize";
+import z from "zod";
+
+const schema = z
+  .object({
+    departureIcao: z
+      .preprocess((x) => (Array.isArray(x) ? x : [x]), z.array(z.string()))
+      .transform((values) => values.map((value) => value.toUpperCase()))
+      .optional(),
+    arrivalIcao: z
+      .preprocess((x) => (Array.isArray(x) ? x : [x]), z.array(z.string()))
+      .transform((values) => values.map((value) => value.toUpperCase()))
+      .optional(),
+    company: z
+      .preprocess((x) => (Array.isArray(x) ? x : [x]), z.array(z.string()))
+      .transform((values) => values.map((value) => value.toUpperCase()))
+      .optional(),
+    aircraftIcaoCode: z
+      .preprocess((x) => (Array.isArray(x) ? x : [x]), z.array(z.string()))
+      .transform((values) => values.map((value) => value.toUpperCase()))
+      .optional(),
+    limit: z.preprocess(
+      (x) => (x ? Number(x) : undefined),
+      z.number().min(1).default(15)
+    ),
+    offset: z.preprocess(
+      (x) => (x ? Number(x) : undefined),
+      z.number().min(0).default(0)
+    ),
+  })
+  .transform(({ aircraftIcaoCode, ...rest }) => {
+    return {
+      ...rest,
+      ...(aircraftIcaoCode && {
+        aircraft: {
+          icaoCode: {
+            [Op.in]: aircraftIcaoCode,
+          },
+        },
+      }),
+    };
+  });
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const data = schema.safeParse(Object.fromEntries(searchParams.entries()));
+
+    if (!data.success) {
+      return NextResponse.json(
+        { message: "Bad Request" },
+        {
+          status: 400,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          },
+        }
+      );
+    }
+
+    const { limit, offset, ...where } = data.data;
+
+    const { count, rows: items } = await FlightModel.findAndCountAll({
+      where,
+      limit,
+      offset,
+      order: ["id"],
+    });
+
+    return NextResponse.json(
+      { count, items },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      {
+        status: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    );
+  }
+}
