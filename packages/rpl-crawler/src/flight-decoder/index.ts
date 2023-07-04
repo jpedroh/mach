af2b21b8c526a55662c0141010dd6c1b1fbbd1a0
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-07-03 21:11:55 -0300

    refactor: use native node uuid function

--- packages/rpl-crawler/src/flight-decoder/index.ts
+++ packages/rpl-crawler/src/flight-decoder/index.ts
@@ -1,6 +1,4 @@
-import makeFlightDecoder from './flight-decoder'
-import { v5 as uuid } from 'uuid'
+import { randomUUID } from "crypto";
+import makeFlightDecoder from "./flight-decoder";
 
-const UUID_NAMESPACE = '618b3d5e-c13f-48e3-ac4c-abb943117b83'
-
-export default makeFlightDecoder({ uuid: line => uuid(line, UUID_NAMESPACE) })
+export default makeFlightDecoder({ uuid: () => randomUUID() });
