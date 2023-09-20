9332d62ecb702146607b02ca999b49c1dcd939ca
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-09-19 22:57:45 -0300

    style: run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [
     {
       env: {
