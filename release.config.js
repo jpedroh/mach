566ead7fde5f7def3a8ec67431c0743771e14543
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-11-20 10:57:11 -0300

    chore: Remove github plugin

--- release.config.js
+++ release.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
     '@semantic-release/git',
   ],
   preset: 'angular',
