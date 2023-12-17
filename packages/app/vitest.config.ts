77be11ac93a6352a18af6e49747af6e2f9fd7aec
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-17 16:38:23 -0300

    test: Fix nx vite tspaths

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nxViteTsPaths()],
   test: {
     environment: 'jsdom',
   },
