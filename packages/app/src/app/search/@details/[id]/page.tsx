a3d4dd64019da437c917cc9ceb2f0bf562fed5e2
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-17 22:47:39 -0300

    fix: Incorrect runtime

--- packages/app/src/app/search/@details/[id]/page.tsx
+++ packages/app/src/app/search/@details/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { FlightDetailsModal } from '@mach/web/details/server'
 
+export const runtime = 'edge';
+
 export default async function Page({ params }) {
   return <FlightDetailsModal id={params.id} />
 }
