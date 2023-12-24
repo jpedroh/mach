af751d4dcca4b1519e4c25f3e9bb21019f4ee7d9
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-24 11:10:14 -0300

    style: Format code

--- packages/app/src/app/global-error.tsx
+++ packages/app/src/app/global-error.tsx
@@ -1,17 +1,17 @@
-"use client";
+'use client'
 
-import * as Sentry from "@sentry/nextjs";
-import NextError from "next/error";
-import { useEffect } from "react";
+import * as Sentry from '@sentry/nextjs'
+import NextError from 'next/error'
+import { useEffect } from 'react'
 
 export default function GlobalError({
   error,
 }: {
-  error: Error & { digest?: string };
+  error: Error & { digest?: string }
 }) {
   useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    Sentry.captureException(error)
+  }, [error])
 
   return (
     <html>
@@ -19,5 +19,5 @@ export default function GlobalError({
         <NextError statusCode={500} />
       </body>
     </html>
-  );
+  )
 }
