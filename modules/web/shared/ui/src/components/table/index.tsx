80c46edf5c73ebe44f0d7bb2353150e3c51cf3f5
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-28 11:57:36 -0300

    refactor: alter table component to use twc

--- modules/web/shared/ui/src/components/table/index.tsx
+++ modules/web/shared/ui/src/components/table/index.tsx
@@ -1,41 +1,13 @@
-import { ReactNode } from 'react'
+import { ComponentProps } from 'react'
+import { twc } from 'react-twc'
 
-export function Table({ children }: { children: ReactNode }) {
-  return <table className={'table-fixed w-full'}>{children}</table>
-}
-
-Table.Head = function TableHead({ children }: { children: ReactNode }) {
-  return (
-    <thead className="bg-blue-700 text-white uppercase">
-      <tr>{children}</tr>
-    </thead>
-  )
-}
-
-Table.Heading = function TableHeading({ children }: { children: ReactNode }) {
-  return <th className="font-semibold p-2">{children}</th>
-}
-
-Table.Body = function TableBody({ children }: { children: ReactNode }) {
-  return (
-    <tbody className="bg-gray-300 bg-opacity-10 dark:text-white uppercase">
-      {children}
-    </tbody>
-  )
-}
-
-Table.Row = function TableRow({ children }: { children: ReactNode }) {
-  return <tr>{children}</tr>
-}
-
-Table.Column = function TableColumn({
-  children,
-  className = '',
-  ...rest
-}: { children: ReactNode } & React.HTMLAttributes<HTMLTableColElement>) {
-  return (
-    <td className={`p-2 text-center ${className}`} {...rest}>
-      {children}
-    </td>
-  )
+export const Table = {
+  Root: twc.table<ComponentProps<'table'>>`table-fixed w-full`,
+  Head: twc.thead<ComponentProps<'thead'>>`bg-blue-700 text-white uppercase`,
+  Heading: twc.th<ComponentProps<'th'>>`font-semibold p-2`,
+  Body: twc.tbody<
+    ComponentProps<'tbody'>
+  >`bg-gray-300 bg-opacity-10 dark:text-white uppercase`,
+  Row: twc.tr<ComponentProps<'tr'>>``,
+  Column: twc.td<ComponentProps<'td'>>`p-2 text-center`,
 }
