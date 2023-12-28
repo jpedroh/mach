2efc6a4463cca4c55fd681cf8aa84ffed2d9689c
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-28 11:44:49 -0300

    refactor: simplify button code by using cva

--- modules/web/shared/ui/src/components/button/index.tsx
+++ modules/web/shared/ui/src/components/button/index.tsx
@@ -1,23 +1,28 @@
+import { VariantProps, cva } from 'class-variance-authority'
 import {
   Button as RACButton,
   ButtonProps as RACButtonProps,
 } from 'react-aria-components'
 import { twc } from 'react-twc'
 
-const VARIANTS = ['primary', 'danger'] as const
+const button = cva(
+  'text-center w-full text-white p-2 rounded-lg focus:ring-4 focus:ring-opacity-50 focus:outline-none disabled:opacity-75 ease-linear transition-all duration-150',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-blue-600 hover:bg-blue-700 focus:ring-blue-800',
+        danger: 'bg-red-600 hover:bg-red-700 focus:ring-red-800',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+    },
+  }
+)
 
-type Variant = (typeof VARIANTS)[number]
+type Props = RACButtonProps &
+  VariantProps<typeof button> & { asChild?: boolean }
 
-type Props = RACButtonProps & { variant?: Variant; asChild?: boolean }
-
-export const Button = twc(RACButton)(({ variant = 'primary' }: Props) => {
-  return [
-    'text-center w-full text-white p-2 rounded-lg focus:ring-4 focus:ring-opacity-50 focus:outline-none disabled:opacity-75 ease-linear transition-all duration-150',
-    variant === 'primary'
-      ? 'bg-blue-600 hover:bg-blue-700 focus:ring-blue-800'
-      : '',
-    variant === 'danger'
-      ? 'bg-red-600 hover:bg-red-700 focus:ring-red-800'
-      : '',
-  ].join(' ')
-})
+export const Button = twc(RACButton)(({ variant }: Props) =>
+  button({ variant })
+)
