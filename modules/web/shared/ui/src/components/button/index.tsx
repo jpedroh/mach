2ce70cfcf74b24941de5793a331e78adee08c9a7
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-28 11:39:27 -0300

    refactor: rewrite button component to use twc

--- modules/web/shared/ui/src/components/button/index.tsx
+++ modules/web/shared/ui/src/components/button/index.tsx
@@ -1,33 +1,23 @@
-import { Slot } from '@radix-ui/react-slot'
-import { ComponentPropsWithoutRef, ReactNode } from 'react'
-import { Button as ReactAriaButton } from 'react-aria-components'
+import {
+  Button as RACButton,
+  ButtonProps as RACButtonProps,
+} from 'react-aria-components'
+import { twc } from 'react-twc'
 
 const VARIANTS = ['primary', 'danger'] as const
 
 type Variant = (typeof VARIANTS)[number]
 
-type Props = { variant?: Variant; className?: string; children: ReactNode } & (
-  | { asChild: true }
-  | ({ asChild?: false } & Omit<
-      ComponentPropsWithoutRef<typeof ReactAriaButton>,
-      'slot'
-    >)
-)
+type Props = RACButtonProps & { variant?: Variant; asChild?: boolean }
 
-export function Button({
-  asChild,
-  variant = 'primary',
-  className = '',
-  ...rest
-}: Props) {
-  const Comp = asChild ? Slot : ReactAriaButton
-
-  const derivedClassName = [
+export const Button = twc(RACButton)(({ variant = 'primary' }: Props) => {
+  return [
     'text-center w-full text-white p-2 rounded-lg focus:ring-4 focus:ring-opacity-50 focus:outline-none disabled:opacity-75 ease-linear transition-all duration-150',
-    'data-[variant=primary]:bg-blue-600 data-[variant=primary]:hover:bg-blue-700 data-[variant=primary]:focus:ring-blue-800',
-    'data-[variant=danger]:bg-red-600 data-[variant=danger]:hover:bg-red-700 data-[variant=danger]:focus:ring-red-800',
-    className,
+    variant === 'primary'
+      ? 'bg-blue-600 hover:bg-blue-700 focus:ring-blue-800'
+      : '',
+    variant === 'danger'
+      ? 'bg-red-600 hover:bg-red-700 focus:ring-red-800'
+      : '',
   ].join(' ')
-
-  return <Comp className={derivedClassName} data-variant={variant} {...rest} />
-}
+})
