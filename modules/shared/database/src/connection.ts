69f33b4791251c67b27c9027a877db7d442afbae
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2024-03-03 00:35:36 -0300

    refactor: alter drizzle connection to use new API (#793)

--- modules/shared/database/src/connection.ts
+++ modules/shared/database/src/connection.ts
@@ -1,31 +1,21 @@
+import { Client } from '@planetscale/database'
 import { drizzle } from 'drizzle-orm/planetscale-serverless'
-import { connect } from '@planetscale/database'
-import * as schema from './schema'
 import { z } from 'zod'
+import * as schema from './schema'
 
-const credentialsSchema = z
-  .object({
-    DATABASE_HOST: z.string(),
-    DATABASE_USERNAME: z.string(),
-    DATABASE_PASSWORD: z.string(),
-  })
-  .transform(({ DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USERNAME }) => {
-    return {
-      host: DATABASE_HOST,
-      username: DATABASE_USERNAME,
-      password: DATABASE_PASSWORD,
-    }
-  })
-
-const connection = connect({
-  ...credentialsSchema.parse(process.env),
-  fetch: (url, init) => {
-    delete init['cache'] // Remove cache header
-    return fetch(url, init)
-  },
+const credentialsSchema = z.object({
+  DATABASE_HOST: z.string(),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
 })
 
-export const db = drizzle(connection, {
-  schema,
-  logger: true,
-})
+const credentials = credentialsSchema.parse(process.env)
+
+export const db = drizzle(
+  new Client({
+    host: credentials.DATABASE_HOST,
+    username: credentials.DATABASE_USERNAME,
+    password: credentials.DATABASE_PASSWORD,
+  }),
+  { schema }
+)
