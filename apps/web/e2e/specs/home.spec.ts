9a57d249fbf52feb2537c5aa9bdbfd5f9cb9c38f
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-03-23 00:10:59 -0300

    test: one more arrow down

--- apps/web/e2e/specs/home.spec.ts
+++ apps/web/e2e/specs/home.spec.ts
@@ -29,5 +29,6 @@ test('if I select options and click search flights it redirects me to search res
 async function selectComboboxValue(page: Page, name: RegExp, value: string) {
   await page.getByRole('combobox', { name }).fill(value)
   await page.getByRole('combobox', { name }).press('ArrowDown')
+  await page.getByRole('combobox', { name }).press('ArrowDown')
   await page.getByRole('combobox', { name }).press('Enter')
 }
