22a729af494326f4a059af382d826cff172afa33
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-04-12 23:54:16 -0300

    feat: add indexes for faster searchs

--- modules/shared/database/src/schema.ts
+++ modules/shared/database/src/schema.ts
@@ -1,33 +1,50 @@
 import { relations } from 'drizzle-orm'
 import { Weekdays, flightRules, wakeTurbulence } from './enum'
-import { int, text, sqliteTable } from 'drizzle-orm/sqlite-core'
+import { int, text, sqliteTable, index } from 'drizzle-orm/sqlite-core'
 
-export const flights = sqliteTable('flights', {
-  id: text('id', { length: 36 }).primaryKey(),
-  callsign: text('callsign', { length: 7 }).notNull(),
-  beginDate: text('begin_date').notNull(),
-  endDate: text('end_date'),
-  company: text('company', { length: 3 }).notNull(),
-  flightNumber: int('flight_number').notNull(),
-  aircraftIcaoCode: text('aircraft_icao_code'),
-  aircraftEquipment: text('aircraft_equipment'),
-  aircraftWakeTurbulence: text('aircraft_wake_turbulence', {
-    enum: wakeTurbulence,
-  }),
-  departureIcao: text('departure_icao', { length: 4 }).notNull(),
-  estimatedOffBlockTime: text('estimated_off_block_time', {
-    length: 4,
-  }).notNull(),
-  cruisingSpeed: text('cruising_speed', { length: 5 }).notNull(),
-  weekdays: text('weekdays', { mode: 'json' }).notNull().$type<Weekdays[]>(),
-  cruisingLevel: int('cruising_level').notNull(),
-  route: text('route').notNull(),
-  arrivalIcao: text('arrival_icao', { length: 4 }).notNull(),
-  estimatedEnrouteMinutes: int('estimated_enroute_minutes').notNull(),
-  flightRules: text('flight_rules', { enum: flightRules }).notNull(),
-  remarks: text('remarks').notNull(),
-  cycle: text('cycle').notNull(),
-})
+export const flights = sqliteTable(
+  'flights',
+  {
+    id: text('id', { length: 36 }).primaryKey(),
+    callsign: text('callsign', { length: 7 }).notNull(),
+    beginDate: text('begin_date').notNull(),
+    endDate: text('end_date'),
+    company: text('company', { length: 3 }).notNull(),
+    flightNumber: int('flight_number').notNull(),
+    aircraftIcaoCode: text('aircraft_icao_code'),
+    aircraftEquipment: text('aircraft_equipment'),
+    aircraftWakeTurbulence: text('aircraft_wake_turbulence', {
+      enum: wakeTurbulence,
+    }),
+    departureIcao: text('departure_icao', { length: 4 }).notNull(),
+    estimatedOffBlockTime: text('estimated_off_block_time', {
+      length: 4,
+    }).notNull(),
+    cruisingSpeed: text('cruising_speed', { length: 5 }).notNull(),
+    weekdays: text('weekdays', { mode: 'json' }).notNull().$type<Weekdays[]>(),
+    cruisingLevel: int('cruising_level').notNull(),
+    route: text('route').notNull(),
+    arrivalIcao: text('arrival_icao', { length: 4 }).notNull(),
+    estimatedEnrouteMinutes: int('estimated_enroute_minutes').notNull(),
+    flightRules: text('flight_rules', { enum: flightRules }).notNull(),
+    remarks: text('remarks').notNull(),
+    cycle: text('cycle').notNull(),
+  },
+  (table) => {
+    return {
+      cycleIdx: index('cycle_idx').on(table.cycle),
+      cycleDepartureArrivalCompanyAircraftIdx: index(
+        'cycle_departure_arrival_company_aircraft_idx'
+      ).on(
+        table.cycle,
+        table.departureIcao,
+        table.arrivalIcao,
+        table.company,
+        table.aircraftIcaoCode
+      ),
+    }
+  }
+)
 
 export const flightsRelations = relations(flights, ({ one }) => ({
   departure: one(airports, {
