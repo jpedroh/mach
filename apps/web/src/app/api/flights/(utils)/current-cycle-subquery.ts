3a978ee91a22cee51fc88e72d39bee78dba22047
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-05-07 20:04:32 -0300

    feat: switch nextjs for remix boilerplate

--- apps/web/src/app/api/flights/(utils)/current-cycle-subquery.ts
+++
@@ -1,6 +0,0 @@
-import { cycles, db } from '@mach/shared/database'
-import { sql } from 'drizzle-orm'
-
-export const currentCycleSubquery = db
-  .select({ cycle: sql<string>`MAX(${cycles.cycle})` })
-  .from(cycles)
