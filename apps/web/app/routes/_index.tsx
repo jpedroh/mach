62c0361698f9c329e39a667470aa584c7a41f7a0
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2024-07-03 20:52:57 -0300

    feat: update cache control headers

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 }
 
 export const headers: HeadersFunction = () => ({
-  'Cache-Control': 'public, max-age=300, s-maxage=3600',
+  'Cache-Control': 'public, max-age=300, stale-while-revalidate=3600',
 })
 
 export { HomePage as default, loader } from '@mach/web/home'
