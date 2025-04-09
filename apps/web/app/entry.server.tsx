c9d10b8dabe1e2608383825d06b1e22f8a77b17d
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-04-08 23:25:08 -0300

    fix: lint issues

--- apps/web/app/entry.server.tsx
+++ apps/web/app/entry.server.tsx
@@ -6,7 +6,7 @@
 
 import { isbot } from 'isbot'
 import { renderToReadableStream } from 'react-dom/server'
-import type { AppLoadContext, EntryContext } from 'react-router'
+import type { EntryContext } from 'react-router'
 import { ServerRouter } from 'react-router'
 
 export default async function handleRequest(
