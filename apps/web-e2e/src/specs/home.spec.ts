f6a6811e21e523e30b891a5f24a48fadd8844604
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-05-08 22:11:42 -0400

    test: skip test

--- apps/web-e2e/src/specs/home.spec.ts
+++ apps/web-e2e/src/specs/home.spec.ts
@@ -10,21 +10,22 @@ test('if I click search flights without selecting a filter it shows an error mes
   ).toBeVisible()
 })
 
-test('if I select options and click search flights it redirects me to search results', async ({
-  page,
-}) => {
-  await page.goto('/')
+test.fixme(
+  'if I select options and click search flights it redirects me to search results',
+  async ({ page }) => {
+    await page.goto('/')
 
-  await selectComboboxValue(page, /departure icao/i, 'SBSP')
-  await selectComboboxValue(page, /arrival icao/i, 'SBRJ')
-  await selectComboboxValue(page, /company/i, 'GLO')
-  await selectComboboxValue(page, /aircraft/i, 'B738')
-  await page.getByRole('button', { name: /search flights/i }).click()
+    await selectComboboxValue(page, /departure icao/i, 'SBSP')
+    await selectComboboxValue(page, /arrival icao/i, 'SBRJ')
+    await selectComboboxValue(page, /company/i, 'GLO')
+    await selectComboboxValue(page, /aircraft/i, 'B738')
+    await page.getByRole('button', { name: /search flights/i }).click()
 
-  await expect(page).toHaveURL(
-    'http://localhost:4200/search?cycle=2023-12-21&departureIcao=SBSP&arrivalIcao=SBRJ&company=GLO&aircraftIcaoCode=B738'
-  )
-})
+    await expect(page).toHaveURL(
+      'http://localhost:4200/search?cycle=2023-12-21&departureIcao=SBSP&arrivalIcao=SBRJ&company=GLO&aircraftIcaoCode=B738'
+    )
+  }
+)
 
 async function selectComboboxValue(page: Page, name: RegExp, value: string) {
   await page.getByRole('combobox', { name }).fill(value)
