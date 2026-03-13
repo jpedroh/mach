29e5b9f063edcb382b6e9b70318ff0d7040da2de
Joao Duarte <joao.pedro.hsd@gmail.com>
2026-03-12 21:55:12 -0300

    chore: testing with tsup

---
+++ apps/rpl-crawler-cli/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  platform: 'node',
+  target: 'node18',
+  bundle: true,
+  sourcemap: true,
+  clean: true,
+  noExternal: [/^@mach\//],
+})
